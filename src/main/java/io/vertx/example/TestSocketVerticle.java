9218e0b68d4fe15206f0cef075333bb906fc714c
Yuji Matsuda <y-matsuda@a-tm.co.jp>
2016-09-29 23:22:37 +0900

    websocketのレスポンステスト処理を追加

--- src/main/java/io/vertx/example/TestSocketVerticle.java
+++ src/main/java/io/vertx/example/TestSocketVerticle.java
@@ -1,6 +1,7 @@
 package io.vertx.example;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServer;
 
@@ -11,11 +12,17 @@ public class TestSocketVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
-        HttpServer server = vertx.createHttpServer();
-        server.websocketHandler(websocket -> {
-            System.out.println("Connected!");
-            Buffer buffer = Buffer.buffer().appendString("connected");
-            websocket.write(buffer);
-        }).listen(8080);
+    	HttpServer server = vertx.createHttpServer();
+    	server.websocketHandler(websocket -> {
+    		System.out.println("Connected!");
+    		websocket.handler(new Handler<Buffer>() {
+    			@Override
+    			public void handle(final Buffer data) {
+    				System.out.println("Receive Data From Client: " + data.getString(0, data.length()));
+    				Buffer buffer = Buffer.buffer().appendString("connected");
+    				websocket.write(buffer);
+    			}
+    		});
+    	}).listen(8080);
     }
 }
