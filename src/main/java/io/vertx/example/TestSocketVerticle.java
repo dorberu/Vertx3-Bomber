355b6d4fdcb794b7ffe017a02fc5d14f4d3d34d7
Yuji Matsuda <dorberu@yahoo.co.jp>
2016-10-08 22:55:11 +0900

    handlerIdの追加削除処理を追加

--- src/main/java/io/vertx/example/TestSocketVerticle.java
+++ src/main/java/io/vertx/example/TestSocketVerticle.java
@@ -3,31 +3,45 @@ package io.vertx.example;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.ServerWebSocket;
 
 /**
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
-public class TestSocketVerticle extends AbstractVerticle {
+public class TestSocketVerticle extends AbstractVerticle
+{
+
+    private Map<String, ServerWebSocket> _socketList;
 
     @Override
-    public void start() {
+    public void start()
+    {
         System.out.println("start");
+        _socketList = new HashMap<String, ServerWebSocket>();
+
         HttpServer server = vertx.createHttpServer();
         server.websocketHandler(websocket -> {
-            System.out.println("Connected!");
+
+            String handlerId = RandomStringUtils.random(20, "0123456789abcdefghijklmnopqrstuvwxyz");
+            _socketList.put(handlerId, websocket);
+            System.out.println("handlerId: " + handlerId + " connect.");
+
             websocket.handler(new Handler<Buffer>() {
                 @Override
-                public void handle(final Buffer data) {
+                public void handle(final Buffer data)
+                {
                     ReceivePacket rp = new ReceivePacket(data.getBytes());
                     Map<String, String> receive = rp.getData();
-                    System.out.println("receive: " + receive.get("id") + " : " + receive.get("flag") + " : " + receive.get("日本語のキー"));
+                    System.out.println("handlerId: " + handlerId + " receive: " + receive.get("id") + " : " + receive.get("flag") + " : " + receive.get("日本語のキー"));
 
                     Map<String, String> send = getData();
-                    System.out.println("send: " + send.get("id") + " : " + send.get("flag") + " : " + send.get("日本語のキー"));
+                    System.out.println("handlerId: " + handlerId + " send: " + send.get("id") + " : " + send.get("flag") + " : " + send.get("日本語のキー"));
 
                     SendPacket sp = new SendPacket();
                     sp.Add(send);
@@ -35,6 +49,16 @@ public class TestSocketVerticle extends AbstractVerticle {
                     websocket.write(buffer);
                 }
             });
+
+            websocket.closeHandler(new Handler<Void>() {
+                @Override
+                public void handle(Void event)
+                {
+                    _socketList.remove(handlerId);
+                    System.out.println("handlerId: " + handlerId + " close.");
+                }
+            });
+
         }).listen(8080);
     }
     
